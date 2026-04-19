9c1b5a3d51fffc4326419de5d2772eb3cb55bd11
helison <helisimmaraujo@gmail.com>
2026-04-19 13:05:32 -0300

    alterado

--- src/Bolo.java
+++ src/Bolo.java
@@ -9,7 +9,7 @@ public class Bolo {
                 "despejar a massa na forma",
                 "colocar o bolo no forno",
                 "retirar o bolo do forno"
-//teste
+
         };
         int etapaAtual = 0;
 
@@ -40,7 +40,7 @@ public class Bolo {
             System.out.println(" vc montou o bolo");
 
         }else{
-            System.out.println(" vc perdeu nao conseguiu fazer o bolo o bolo queimou ");
+            System.out.println(" vc perdeu nao conseguiu fazer o bolo");
         }
     }
 }
